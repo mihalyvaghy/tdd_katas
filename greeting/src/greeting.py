0c4fdc3460c3054ee49bb744d8394a08cd4e136e
Mihály Vághy <vaghy.mihaly@gmail.com>
2020-12-17 01:28:01 +0100

    Greeting kata finished

--- greeting/src/greeting.py
+++ greeting/src/greeting.py
@@ -1,21 +1,64 @@
-def greet(name):
-    greeting = 'Hello'
-    conjunction = 'and'
-    print_name = name
+def clean_name(name):
+    if name[0] == "\"":
+        name = name[1:]
+    if name[-1] == "\"":
+        name = name[:-1]
+    return name
 
-    oxford_comma = lambda name_list: len(name_list) > 2
+def clean_names(names):
+    return [clean_name(name) for name in names]
 
-    if isinstance(name, list):
-        if False not in map(lambda string: string.isupper(), name):
-            greeting = 'HELLO'
-            conjunction = 'AND'
-        print_name = ', '.join(name[:-1]) + (',' if oxford_comma(name) else '') + ' ' + conjunction + ' ' + name[-1] 
+def oxford_comma(names):
+    return "," if len(names) > 2 else ""
 
-    elif name:
-        if name.isupper():
-            greeting = 'HELLO'
+def join_names(greeting, names, conjunction):
+    return greeting + ", " + ", ".join(names[:-1]) + oxford_comma(names) + " " + conjunction + " " + names[-1] + "!"
 
+def greet_lower_name(name):
+    return "Hello, " + name + "!"
+
+def greet_upper_name(name):
+    return "HELLO, " + name + "!"
+
+def greet_lower_list(names):
+    return join_names("Hello", names, "and") if len(names) > 0 else ""
+
+def greet_upper_list(names):
+    return join_names("HELLO", names, "AND") if len(names) > 0 else ""
+
+def greet_lower(names):
+    if len(names) > 1:
+        return greet_lower_list(names)
+    elif len(names) == 1:
+        return greet_lower_name(names[0])
     else:
-        print_name = 'my friend'
+        return ""
 
-    return greeting + ', ' + print_name + '!'
+def greet_upper(names):
+    if len(names) > 1:
+        return greet_upper_list(names)
+    elif len(names) == 1:
+        return greet_upper_name(names[0])
+    else:
+        return ""
+
+def greet_list(names):
+    lower_names = [name for name in names if not name.isupper()]
+    upper_names = [name for name in names if name.isupper()]
+    return greet_lower(lower_names) + (" AND " if len(lower_names) > 0 and len(upper_names) > 0 else "") + greet_upper(upper_names) 
+
+def greet_name(name):
+    if name:
+        if not name.isupper():
+            return "Hello, " + name + "!"
+        else:
+            return "HELLO, " + name + "!"
+    else:
+        return "Hello, my friend!"
+
+def greet(names):
+    if isinstance(names, list):
+        names = clean_names(names)
+        return greet_list(names)
+    else:
+        return greet_name(names)
--- greeting/test/test_greeting.py
+++ greeting/test/test_greeting.py
@@ -3,25 +3,31 @@ from src import greeting
 
 class GreetingTest(unittest.TestCase):
     def test_properName(self):
-        self.assertEqual('Hello, Mitya!', greeting.greet('Mitya'))
+        self.assertEqual("Hello, Mitya!", greeting.greet("Mitya"))
 
     def test_nullName(self):
-        self.assertEqual('Hello, my friend!', greeting.greet(None))
+        self.assertEqual("Hello, my friend!", greeting.greet(None))
 
     def test_shouting(self):
-        self.assertEqual('HELLO, MITYA!', greeting.greet('MITYA'))
+        self.assertEqual("HELLO, MITYA!", greeting.greet("MITYA"))
 
     def test_twoNames(self):
-        self.assertEqual('Hello, Bia and Mitya!', greeting.greet(['Bia', 'Mitya']))
+        self.assertEqual("Hello, Bia and Mitya!", greeting.greet(["Bia", "Mitya"]))
 
     def test_twoShouting(self):
-        self.assertEqual('HELLO, BIA AND MITYA!', greeting.greet(['BIA', 'MITYA']))
+        self.assertEqual("HELLO, BIA AND MITYA!", greeting.greet(["BIA", "MITYA"]))
 
     def test_manyNames(self):
-        self.assertEqual('Hello, Bia, Mitya, and Písz!', greeting.greet(['Bia', 'Mitya', 'Písz']))
+        self.assertEqual("Hello, Bia, Mitya, and Písz!", greeting.greet(["Bia", "Mitya", "Písz"]))
 
     def test_manyShouting(self):
-        self.assertEqual('HELLO, BIA, MITYA, AND PÍSZ!', greeting.greet(['BIA', 'MITYA', 'PÍSZ']))
+        self.assertEqual("HELLO, BIA, MITYA, AND PÍSZ!", greeting.greet(["BIA", "MITYA", "PÍSZ"]))
 
-if __name__ == '__main__':
+    def test_mixedShouting(self):
+        self.assertEqual("Hello, Bia and Mitya! AND HELLO, PÍSZ!", greeting.greet(["Bia", "Mitya", "PÍSZ"]))
+
+    def test_escapeComma(self):
+        self.assertEqual("Hello, Bia and Mitya, Písz!", greeting.greet(["Bia", "\"Mitya, Písz\""]))
+
+if __name__ == "__main__":
     unittest.main()
