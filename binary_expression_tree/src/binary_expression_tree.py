282b97e54942db94d8ef7e2af013eaacfb298d45
Mihály Vághy <vaghy.mihaly@gmail.com>
2020-12-21 10:58:18 +0100

    Infix to postfix conversion done

--- binary_expression_tree/src/binary_expression_tree.py
+++ binary_expression_tree/src/binary_expression_tree.py
@@ -2,5 +2,69 @@ import re
 
 class BinExpTree:
     @staticmethod
+    def __is_operator(char):
+        return char in "+-*/^"
+
+    @staticmethod
+    def __is_operand(char):
+        return not BinExpTree.__is_operator(char) and char not in "()"
+
+    @staticmethod
+    def __precedence(operator):
+        return {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3, "(": -1, ")": -1}[operator]
+
+    @staticmethod
+    def __empty_stack(stack):
+        return len(stack) == 0
+
+    @staticmethod
+    def __higher_precedence(operator_1, operator_2):
+        return BinExpTree.__precedence(operator_1) >= BinExpTree.__precedence(operator_2)
+
+    @staticmethod
+    def __pop_higher_precedence(stack, postfix, operator):
+        while not BinExpTree.__empty_stack(stack) and BinExpTree.__higher_precedence(stack[-1], operator):
+            postfix.append(stack.pop())
+        return stack, postfix
+
+    @staticmethod
+    def __pop_parenthesis(stack, postfix):
+        while stack[-1] != "(":
+            postfix.append(stack.pop())
+        stack.pop()
+        return stack, postfix
+
+    @staticmethod
+    def infix_to_postfix_stack(expression):
+        postfix = []
+        stack = []
+        tmp_operand = ""
+        infix = re.split("(\+|-|\*|/|\^|\(|\))", expression)
+        for word in infix:
+            if word == "":
+                continue
+            elif BinExpTree.__is_operand(word):
+                postfix.append(word)
+            elif BinExpTree.__is_operator(word):
+                if BinExpTree.__empty_stack(stack) or stack[-1] == "(" or BinExpTree.__higher_precedence(word, stack[-1]):
+                    stack.append(word)
+                else:
+                    stack, postfix = BinExpTree.__pop_higher_precedence(stack, postfix, word)
+                    stack.append(word)
+            elif word == "(":
+                stack.append(word)
+            elif word == ")":
+                stack, postfix = BinExpTree.__pop_parenthesis(stack, postfix)
+        while len(stack) != 0:
+            postfix.append(stack.pop())
+        return postfix
+
+    @staticmethod
     def infix_to_postfix(expression):
-        objects = re.split("
+        return "".join(BinExpTree.infix_to_postfix_stack(expression))
+
+    def __init__(self, expression):
+        pass
+
+    def print(self):
+        pass
--- binary_expression_tree/test/test_binary_expression_tree.py
+++ binary_expression_tree/test/test_binary_expression_tree.py
@@ -5,3 +5,10 @@ class BinExpTreeTest(unittest.TestCase):
     def test_infix_to_postfix_conversion_no_parentheses(self):
         self.assertEqual("12+", BinExpTree.infix_to_postfix("1+2"))
         self.assertEqual("12*3+", BinExpTree.infix_to_postfix("1*2+3"))
+
+    def test_infix_to_postfix_conversion(self):
+        self.assertEqual("abcd^e-fgh*+^*+i-", BinExpTree.infix_to_postfix("a+b*(c^d-e)^(f+g*h)-i"))
+
+    def test_binary_expression_tree_print_no_parentheses(self):
+        self.assertEqual("1+2", BinExpTree("1+2").print())
+        self.assertEqual("1*2+3", BinExpTree("1*2+3").print())
